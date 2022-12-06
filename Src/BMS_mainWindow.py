ad861710b2789ef1865654b9d58fd11a468fd256
Zyuangeorge <georgeyz365@gmail.com>
2022-12-06 17:43:48 +0000

    update

--- Src/BMS_mainWindow.py
+++ Src/BMS_mainWindow.py
@@ -601,7 +601,7 @@ class mainWindow(QMainWindow, Ui_MainWindow):
 
             self.outputData = np.append(self.outputData, [realTimeData], axis = 0) # Convert to two dimension and add to output data
             
-            if self.outputData.shape[0] > 10: # Automatic Recording
+            if self.outputData.shape[0] > 3600: # Automatic Recording
                 columnName = [
                             'cellVoltage_1','cellVoltage_2','cellVoltage_3','cellVoltage_4',
                             'cellVoltage_5', 'cellVoltage_6', 'cellVoltage_7', 'cellVoltage_8', 
