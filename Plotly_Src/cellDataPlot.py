3465e1c18c9d2d39d6f297d3faa3cc969bde98df
Zyuangeorge <georgeyz365@gmail.com>
2023-03-26 23:23:26 +0100

    improve data plotting

--- Plotly_Src/cellDataPlot.py
+++ Plotly_Src/cellDataPlot.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import plotly.graph_objects as go
+import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
 import pandas as pd
@@ -39,27 +40,16 @@ class cellDataPlotting:
             batteryData['cellSoH_' + str(batteryNumber + 1)] = batteryData['cellSoH_' + str(
                 batteryNumber + 1)].map(lambda x: x / 10)
 
+        batteryData = batteryData[0:5300] # [0:5300] is for cell balancing data
+
         return batteryData
 
     def plotBatteryData(self):
         """Plot multiple battery data"""
-        df = self.prepareDataSet()
-        batteryData = df[0:5300]
+        batteryData = self.prepareDataSet()
 
         fig = make_subplots(rows=2, cols=2, subplot_titles=(
             "Pack Current", 'Cell Voltage', 'Cell SoC', 'Cell SoH', 'Cell CB Control'))
-
-        # Update xaxis properties
-        fig.update_xaxes(title_text="Time(s)", row=1, col=1)
-        fig.update_xaxes(title_text="Time(s)", row=1, col=2)
-        fig.update_xaxes(title_text="Time(s)", row=2, col=1)
-        fig.update_xaxes(title_text="Time(s)", row=2, col=2)
-
-        # Update yaxis properties
-        fig.update_yaxes(title_text="Current (mA)", row=1, col=1)
-        fig.update_yaxes(title_text="Voltage (mV)", row=1, col=2)
-        fig.update_yaxes(title_text="SoC (%)", row=2, col=1)
-        fig.update_yaxes(title_text="SoH (%)", row=2, col=2)
 
         fig.add_trace(go.Scatter(x=batteryData['Time(s)'], y=batteryData['packCurrent'],
                                  mode='lines',
@@ -82,10 +72,22 @@ class cellDataPlotting:
                                      name='cellSoH_' + str(batteryNumber + 1)),
                           row=2, col=2)
 
+        fig = self.changeStyle(fig, 'Time(s)', "Current (mA)", False)
+
+        # Update xaxis properties
+        fig.update_xaxes(title_text="Time(s)", row=1, col=1)
+        fig.update_xaxes(title_text="Time(s)", row=1, col=2)
+        fig.update_xaxes(title_text="Time(s)", row=2, col=1)
+        fig.update_xaxes(title_text="Time(s)", row=2, col=2)
+
+        # Update yaxis properties
+        fig.update_yaxes(title_text="Current (mA)", row=1, col=1)
+        fig.update_yaxes(title_text="Voltage (mV)", row=1, col=2)
+        fig.update_yaxes(title_text="SoC (%)", row=2, col=1)
+        fig.update_yaxes(title_text="SoH (%)", row=2, col=2)
+
         fig.update_layout(
-            title='Battery Data' + self.folder[6:],
-            xaxis_title='Time(s)',
-            hovermode='x'
+            hovermode='x',
         )
 
         fig.show()
@@ -102,22 +104,151 @@ class cellDataPlotting:
 
             trace = go.Scatter(x=batteryData['Time(s)'], y=y,
                                mode='lines', name='cellCB_' + str(batteryNumber + 1))
-            
+
             traces.append(trace)
 
-        fig = make_subplots(rows=14, cols=1, shared_xaxes=True, vertical_spacing=0.02)
-
-        fig.update_yaxes(type="category")
+        fig = make_subplots(
+            rows=14, cols=1, shared_xaxes=True, vertical_spacing=0.02)
 
         for i, trace in enumerate(traces):
             fig.add_trace(trace, row=i+1, col=1)
 
-        fig.update_layout(
-            title="Cell Balancing Control Signal")
+        fig = self.changeStyle(
+            fig, "Time (s)", "Cell Balancing Control (On/Off)", True)
 
+        newnames = {'cellCB_1': 'Cell 1', 'cellCB_2': 'Cell 2', 'cellCB_3': 'Cell 3', 'cellCB_4': 'Cell 4', 'cellCB_5': 'Cell 5',
+                    'cellCB_6': 'Cell 6', 'cellCB_7': 'Cell 7', 'cellCB_8': 'Cell 8', 'cellCB_9': 'Cell 9', 'cellCB_10': 'Cell 10',
+                    'cellCB_11': 'Cell 11', 'cellCB_12': 'Cell 12', 'cellCB_13': 'Cell 13', 'cellCB_14': 'Cell 14'}
+        fig.for_each_trace(lambda t: t.update(name=newnames[t.name]))
         fig.show()
+
+        fig2 = go.Figure()
+
+        for batteryNumber in range(14):
+            fig2.add_trace(go.Scatter(x=batteryData['Time(s)'], y=batteryData['cellVoltage_' + str(batteryNumber + 1)],
+                                      mode='lines',
+                                      name='cellVoltage_' + str(batteryNumber + 1)))
+
+        fig2 = self.changeStyle(fig2, "Time (s)", "Cell Voltage (mV)", False)
+
+        newnames = {'cellVoltage_1': 'Cell 1', 'cellVoltage_2': 'Cell 2', 'cellVoltage_3': 'Cell 3', 'cellVoltage_4': 'Cell 4', 'cellVoltage_5': 'Cell 5',
+                    'cellVoltage_6': 'Cell 6', 'cellVoltage_7': 'Cell 7', 'cellVoltage_8': 'Cell 8', 'cellVoltage_9': 'Cell 9', 'cellVoltage_10': 'Cell 10',
+                    'cellVoltage_11': 'Cell 11', 'cellVoltage_12': 'Cell 12', 'cellVoltage_13': 'Cell 13', 'cellVoltage_14': 'Cell 14'}
+        fig2.for_each_trace(lambda t: t.update(name=newnames[t.name]))
+        fig2.show()
+
+        # Filter data distorted by the balancing process
+        fig3 = go.Figure()
+        trendData = batteryData
+        trendData.loc[batteryData['cellCB_9'] != 0, :] = np.nan
+
+        for batteryNumber in range(14):
+            dataColumn = trendData['cellVoltage_' + str(batteryNumber + 1)]
+            
+            # Filter outlier 
+            mean = dataColumn.mean()
+            sigma = dataColumn.std()
+
+            trendData = trendData.loc[(dataColumn >= mean - 3 * sigma) & (
+                dataColumn <= mean + 3 * sigma)]
+
+            trendData.fillna(method='bfill')
+
+            # Median filter
+            trendData.loc[:, 'filteredCellVoltage_' + str(batteryNumber + 1)] = trendData.loc[:, 'cellVoltage_' + str(batteryNumber + 1)].rolling(65, center=True).median().values
+
+            fig3.add_trace(go.Scatter(x=trendData['Time(s)'], y=trendData['filteredCellVoltage_' + str(batteryNumber + 1)],
+                                      mode='lines',
+                                      name='filteredCellVoltage_' + str(batteryNumber + 1)))
+
+        fig3 = self.changeStyle(fig3, "Time (s)", "Cell Voltage (mV)", False)
+
+        newnames = {'filteredCellVoltage_1': 'Cell 1', 'filteredCellVoltage_2': 'Cell 2', 'filteredCellVoltage_3': 'Cell 3', 'filteredCellVoltage_4': 'Cell 4', 'filteredCellVoltage_5': 'Cell 5',
+                    'filteredCellVoltage_6': 'Cell 6', 'filteredCellVoltage_7': 'Cell 7', 'filteredCellVoltage_8': 'Cell 8', 'filteredCellVoltage_9': 'Cell 9', 'filteredCellVoltage_10': 'Cell 10',
+                    'filteredCellVoltage_11': 'Cell 11', 'filteredCellVoltage_12': 'Cell 12', 'filteredCellVoltage_13': 'Cell 13', 'filteredCellVoltage_14': 'Cell 14'}
+        fig3.for_each_trace(lambda t: t.update(name=newnames[t.name]))
+        fig3.show()
+
+    def changeStyle(self, fig, xLabel, yLabel, stackPlot):
+        # choose the figure font
+        font_dict = dict(family='Times New Roman',
+                         size=18,
+                         color='black'
+                         )
+
+        # general figure formatting
+        fig.update_layout(font=font_dict,  # font formatting
+                          plot_bgcolor='white',  # background color
+                          # width=850,  # figure width
+                          # height=700,  # figure height
+                          # margin=dict(r=20, t=20, b=10)  # remove white space
+                          legend=dict(
+                              title_font_family="Times New Roman",
+                              font=font_dict,
+                              bgcolor="White",
+                              bordercolor="Black",
+                              borderwidth=1
+                          )
+                          )
+
+        if stackPlot == False:
+            # x and y-axis formatting
+            fig.update_yaxes(title_text=yLabel,  # axis label
+                             showline=True,  # add line at x=0
+                             linecolor='black',  # line color
+                             linewidth=2.4,  # line size
+                             ticks='outside',  # ticks outside axis
+                             tickfont=font_dict,  # tick label font
+                             mirror='allticks',  # add ticks to top/right axes
+                             tickwidth=2.4,  # tick width
+                             tickcolor='black',  # tick color
+                             gridcolor='lightgray'
+                             )
+
+            fig.update_xaxes(title_text=xLabel,
+                             showline=True,
+                             showticklabels=True,
+                             linecolor='black',
+                             linewidth=2.4,
+                             ticks='outside',
+                             tickfont=font_dict,
+                             mirror='allticks',
+                             tickwidth=2.4,
+                             tickcolor='black',
+                             gridcolor='lightgray'
+                             )
+        else:
+            # x and y-axis formatting
+            fig.update_layout(showlegend=True)
+
+            fig.update_yaxes(
+                showline=True,  # add line at x=0
+                linecolor='black',  # line color
+                linewidth=2.4,  # line size
+                mirror='all',  # add ticks to top/right axes
+                type="category",
+            )
+
+            # fig.update_yaxes(ticksuffix='Cell 1', showtickprefix = 'first', row=1, col=1)  # axis label
+            fig.update_yaxes(title_text='Balancing control signal',
+                             row=7, col=1)  # axis label
+
+            fig.update_xaxes(
+                showline=True,
+                showticklabels=False,
+                linecolor='black',
+                linewidth=2.4,
+                mirror='all',
+            )
+
+            fig.update_xaxes(title_text=xLabel,
+                             showticklabels=True, row=14, col=1)
+
+        return fig
+
 
 if __name__ == "__main__":
     cbAndSoCData = 'Data\\Cyclon_2.5Ah_LeadAcid\\24-03-2023'
-    cellDataPlotting(cbAndSoCData).plotBatteryData()
+    charingData = 'Data\\Cyclon_2.5Ah_LeadAcid\\23-03-2023'
+    cellDataPlotting(charingData).plotBatteryData()
     cellDataPlotting(cbAndSoCData).plotCBControlData()
